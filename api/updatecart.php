8b38f5a4bab479695a343ef1a8de982a3a91b189
thasa11 <tarmo.hasa@hotmail.com>
2016-03-06 22:46:42 +0200

    Add stock availability check to update cart function

--- api/updatecart.php
+++ api/updatecart.php
@@ -15,38 +15,52 @@
         $id = $_REQUEST['productid'];
         $userid = 0;
         $count = 0;
+        $saldo = 0;
 
-        // 2 Update cart with new amount
-                
-        $query="UPDATE basket SET pcs='$newcount' WHERE pid=$id;";
-        
-        //echo $query;
+        // 2 Check product availability
+        $query="SELECT amount - reserved as saldo FROM stock WHERE productid = $id;";
         $result=$mysqli->query($query);
-        if ($mysqli->errno) $myArray[0]= '<span>'.$mysqli->error.':'.$mysqli->errno.'</span>';
-        else {
-            $myArray[0]= "<span>Ostoskori päivitetty ".$mysqli->affected_rows.' kpl</span>';
-            $myArray[1]= $id;
-        }
-        
-        // 2 Get count of product reserved in all baskets
-        // TODO: userid not used
-        $query="SELECT sum(pcs) as summa FROM basket WHERE pid = $id and purchased = 'N';";
-        // echo $query;
-        $result=$mysqli->query($query);
-        if ($row = $result->fetch_object()){
-            $count = $row->summa;
-        }
+        $row = $result->fetch_object();
+        $saldo = $row->saldo;
 
-        // 3 Update stock reserved with sum of basket content
-        $query="UPDATE stock SET reserved=$count WHERE productid=$id;";
-        $result=$mysqli->query($query);
-        // echo $query;
-        $result=$mysqli->query($query);
-        if ($mysqli->errno) $myArray[0]= '<span>'.$mysqli->error.':'.$mysqli->errno.'</span>';
-        else {
-            $myArray[0]= "<span>Varastotiedot päivitetty ".$mysqli->affected_rows.' kpl</span>';
-            $myArray[1]= $id;
-        }                
+        // 2.1 Notice about stock availability
+        if ($newcount > $saldo){
+                $myArray[0]= "</br><span>Tuotteen varastosaldo ".$saldo." ei riitä!</span>";
+                $myArray[1]= $id;
+        } else {
+
+            // 2 Update cart with new amount
+                    
+            $query="UPDATE basket SET pcs='$newcount' WHERE pid=$id;";
+            
+            //echo $query;
+            $result=$mysqli->query($query);
+            if ($mysqli->errno) $myArray[0]= '<span>'.$mysqli->error.':'.$mysqli->errno.'</span>';
+            else {
+                $myArray[0]= "<span>Ostoskori päivitetty ".$mysqli->affected_rows.' kpl</span>';
+                $myArray[1]= $id;
+            }
+            
+            // 2 Get count of product reserved in all baskets
+            // TODO: userid not used
+            $query="SELECT sum(pcs) as summa FROM basket WHERE pid = $id and purchased = 'N';";
+            // echo $query;
+            $result=$mysqli->query($query);
+            if ($row = $result->fetch_object()){
+                $count = $row->summa;
+            }
+    
+            // 3 Update stock reserved with sum of basket content
+            $query="UPDATE stock SET reserved=$count WHERE productid=$id;";
+            $result=$mysqli->query($query);
+            // echo $query;
+            $result=$mysqli->query($query);
+            if ($mysqli->errno) $myArray[0]= '<span>'.$mysqli->error.':'.$mysqli->errno.'</span>';
+            else {
+                $myArray[0]= "<span>Varastotiedot päivitetty ".$mysqli->affected_rows.' kpl</span>';
+                $myArray[1]= $id;
+            }
+        }
                 
         // 6 Json encode -- allow cross site script loading content
         header('Content-Type: application/json');
